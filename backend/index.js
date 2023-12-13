53d65bafbc429c1efdf1f19ecdde93acc1198a08
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-13 12:07:46 +0100

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on PORT: ${process.env.PORT}`);
+});
